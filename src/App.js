4511308dd3fb5ca417061354ff6241353f83fc06
Camo282 <camo282@gmail.com>
2022-10-03 14:20:52 -0400

    successfully ran tests

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
